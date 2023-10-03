84d8e63ee0cdf3a1a078c33547d4095ea54649e3
okoeee <yk27.puxcm@gmail.com>
2023-10-03 21:51:14 +0900

    add settings to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { NgxsModule } from '@ngxs/store';
+import { TodoState } from './todo/todo.state';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxsModule.forRoot([TodoState])
   ],
   providers: [],
   bootstrap: [AppComponent]
