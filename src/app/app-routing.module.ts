87c23825b9999c748e3c32c66e6a8b0ca34688fb
okoeee <yk27.puxcm@gmail.com>
2023-10-07 16:18:08 +0900

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
-  {path: "", component: TodoListComponent}
+  { path: "", component: TodoListComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
