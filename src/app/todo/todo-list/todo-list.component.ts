f3c43e95ffad6de790d30b901447cf39faa1fc5c
okoeee <yk27.puxcm@gmail.com>
2023-10-07 15:43:27 +0900

    add form ui

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import { TodoModel, TodoState } from '../todo.state';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { TodoAction } from '../todo.actions';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,12 +14,22 @@ export class TodoListComponent {
 
   @Select(TodoState.getState) todoList$?: Observable<TodoModel[]>;
 
+  todoForm = this.formBuilder.group({
+    title: ['', Validators.required]
+  })
+
   constructor(
-    private store: Store
+    private store: Store,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
     this.store.dispatch(new TodoAction.GetAll())
+  }
+
+  onSubmit(): void {
+    const val = this.todoForm.value
+    console.log(val)
   }
 
 }
