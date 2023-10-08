3a5fdbb4351d9510afdd1d7139d3915fd97b6f6c
okoeee <yk27.puxcm@gmail.com>
2023-10-08 09:40:33 +0900

    add login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  loginForm = this.formBuilder.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    
+  }
+
+}
