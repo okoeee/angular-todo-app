cdbcfe57c123fc92ad1f5a6426e07aed4162ffed
okoeee <yk27.puxcm@gmail.com>
2023-10-08 09:28:16 +0900

    add guard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { LoginComponent } from './user/login/login.component';
+import { authGuard } from './auth/gard';
 
 const routes: Routes = [
-  { path: "", component: TodoListComponent },
+  { path: "", component: TodoListComponent, canActivate: [authGuard] },
   { path: "login", component: LoginComponent }
 ];
 
