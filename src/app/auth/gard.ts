d7d132e9968f6365a35983ff280a4af8dcf2b129
okoeee <yk27.puxcm@gmail.com>
2023-10-09 09:53:36 +0900

    update gard

--- src/app/auth/gard.ts
+++ src/app/auth/gard.ts
@@ -1,23 +1,22 @@
 import { inject } from "@angular/core";
 import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Router } from "@angular/router";
-import { catchError, throwError } from "rxjs";
 
 export const authGuard = () => {
   const http = inject(HttpClient)
   const router = inject(Router)
 
-  return http.get("api/verify", { observe: 'response' }).pipe(
-    catchError((error, httpResponse) => {
-      console.error("Un authorized")
-      router.navigate(['/login'])
-      return throwError(error)
-    })
-  ).subscribe(
-    (response: HttpResponse<any>) => {
-      if(response.status === 200) return true
-      return router.parseUrl('/login')
-    }
-  )
+  return http.get("api/verify", { observe: 'response' })
+  .subscribe({
+    next: (_: HttpResponse<any>) => {
+      return router.navigate(["/"])
+    },
+    error: (httpResponse: HttpResponse<any>) => {
+      if(httpResponse.status === 401) return router.navigate(["/login"])
+      console.error(`Verify error reason is ${httpResponse.status}: ${httpResponse.statusText}`)
+      return router.navigate(["/login"])
+    },
+    complete: () => {}
+  })
 
 }
