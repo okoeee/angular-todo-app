398d5bb44e6e6083abd5cebd9355e033ac4f4a99
okoeee <yk27.puxcm@gmail.com>
2023-10-21 21:48:20 +0900

    add logout ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { UserAction } from './user/user.action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(
+    private store: Store
+  ) {}
+
+  logout() {
+    this.store.dispatch(new UserAction.Logout)
+  }
+
+}
